2fc44b38cd01c5ad832cfa9cdb9fd537ac4320b1
bartosz-zukowski <bz.bartosz.zukowski@gmail.com>
2024-04-30 15:16:16 +0200

    Remove not used file

--- tests/testthat/test-L_dla_2_populacji.R
+++ tests/testthat/test-L_dla_2_populacji.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+describe("Liczy poprawnie wspolczynnik L w zadaniu o silnikach)", {
+  it("Poprawnie oblicza srednia arytmetyczna dla proby 2,5,8 rowny X_ar=5", {
+    expect_equal(L_dla_2_populacji(c(5.3, 5.6, 5.6, 5.5, 5.5, 5.4, 5.2), c(5.8, 5.7, 5.5, 5.7, 5.6, 5.9), t_alfa=2.201), 1.795885)
+  })
 })
+
+
+
